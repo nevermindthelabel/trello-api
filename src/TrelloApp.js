f287363aaee91ea10cc7094e97a30d958c7f9eba
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-30 21:29:29 -0700

    cleared out unused html

--- src/TrelloApp.js
+++ src/TrelloApp.js
@@ -1,5 +1,4 @@
 import { LitElement, html, css } from 'lit-element';
-import { openWcLogo } from './open-wc-logo.js';
 
 export class TrelloApp extends LitElement {
   static get properties() {
@@ -23,32 +22,12 @@ export class TrelloApp extends LitElement {
         margin: 0 auto;
         text-align: center;
       }
-
       main {
         flex-grow: 1;
       }
-
-      .logo > svg {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
       .app-footer {
         font-size: calc(12px + 0.5vmin);
         align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
       }
     `;
   }
@@ -56,28 +35,11 @@ export class TrelloApp extends LitElement {
   render() {
     return html`
       <main>
-        <div class="logo">${openWcLogo}</div>
-        <h1>My app</h1>
-
-        <p>Edit <code>src/TrelloApp.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/developing/#code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        things
       </main>
 
       <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
+      stuff
       </p>
     `;
   }
